c7901621500e8ffff602ea3084fbe0498dbb8270
Shtya <115083423+Shtya@users.noreply.github.com>
2022-12-26 10:21:21 +0200

    Update data.jsx

--- src/component/data.jsx
+++ src/component/data.jsx
@@ -2,6 +2,8 @@ const Works = [
   {img : "Dashboard.png", title : "Dashboard" , visit: "https://shtya.github.io/Dashboard/" , github :"https://github.com/Shtya/Dashboard" },
   {img : "Special.png", title : "Design Special" , visit: "https://shtya.github.io/Special_Design/" , github :"https://github.com/Shtya/Special_Design" },
   {img : "Gym.png", title : "Gym" , visit: "https://gym-etg.pages.dev/" , github :"https://github.com/Shtya/GYM" },
+  {img : "Dev.png", title : "Dev" , visit: "https://shtya.github.io/Dev/" , github :"https://github.com/Shtya/Dev" },
+  {img : "Kasper.png", title : "Kasper" , visit: "https://shtya.github.io/Kasper/" , github :"https://github.com/Shtya/Kasper" },
   {img : "Leon.png", title : "Leon" , visit: "https://shtya.github.io/Project-Leon/" , github :"https://github.com/Shtya/Project-Leon" },
 ]
 
